d3b6063575ef9efc9acbbda4b69e0675c4876314
Wayne Thomson <waynet@btinternet.com>
2020-09-13 21:22:11 +0100

    Added Location header for POST requests.

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -3,7 +3,8 @@ package guru.springframework.msscbrewery.web.controller;
 import guru.springframework.msscbrewery.services.CustomerService;
 import guru.springframework.msscbrewery.web.model.CustomerDTO;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping(value = "/api/v1/customer")
@@ -29,9 +31,10 @@ public class CustomerController {
     }
 
     @PostMapping(value = {"/{id}"})
-    @ResponseStatus(HttpStatus.CREATED)
-    public CustomerDTO createCustomer(@PathVariable UUID id, @RequestBody CustomerDTO customer) {
-        return customerService.createCustomer(id, customer);
+    public ResponseEntity<CustomerDTO> createCustomer(@PathVariable UUID id, @RequestBody CustomerDTO customer) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Location", "http://localhost:8080/api/v1/customer/" + id);
+        return new ResponseEntity<>(customerService.createCustomer(id, customer), httpHeaders, CREATED);
     }
 
     @PutMapping({"/{id}"})
